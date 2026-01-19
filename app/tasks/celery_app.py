0207325a2965297d43fa4e7ae8387616ab14b9f4
Nikita <kiselewb@gmail.com>
2026-01-19 16:01:35 +0300

    new commit

---
+++ app/tasks/celery_app.py
@@ -0,0 +1,12 @@
+from celery import Celery
+from app.config import settings
+
+
+celery_instance = Celery("tasks", broker=settings.REDIS_URL, include=["app.tasks.tasks"])
+
+celery_instance.conf.beat_schedule = {
+    "run-every-day": {
+        "task": "booking_today_checkin",
+        "schedule": 5,
+    }
+}
