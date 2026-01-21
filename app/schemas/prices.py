16ab631dc239365d5c40b5612c858b0e389e3832
Nikita <kiselewb@gmail.com>
2026-01-21 11:36:34 +0300

    new commit

--- app/schemas/prices.py
+++ app/schemas/prices.py
@@ -1,11 +1,15 @@
 from datetime import datetime, timezone
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, Field, field_serializer
 from decimal import Decimal
 
 
 class PriceCreate(BaseModel):
     ticker: str
-    price: Decimal
+    price: Decimal = Field(json_schema_extra={"example": "45230.50"})
+
+    @field_serializer('price')
+    def serialize_price(self, price: Decimal) -> str:
+        return f"{price:.2f}"
 
 
 class PriceResponse(PriceCreate):
